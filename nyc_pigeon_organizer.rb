13742d02d492bf72c038a9ea201453c66a187659
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-04-08 18:24:04 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,21 +3,23 @@ require "pry"
 def nyc_pigeon_organizer(data)
 pigeon_data = {}
 
-data.each do |attributes_key, attribute_values_array|
-  attribute_values_array.each do |properties, names|
-    
+data.each do |attribute_keys, attribute_values|
+  attribute_values.each do |properties, names|
     names.each do |name|
-      if !pigeon_data[name] 
+      if !pigeon_data[name]
         pigeon_data[name] = {}
+        #if it is not true create it
+        end 
+        
+      if !pigeon_data[name][attribute_keys]
+        pigeon_data[name][attribute_keys] = []
+        #if its not true, create it
+          end 
+          binding.pry
+          pigeon_data[name][attribute_keys] << properties.to_s 
+          #conert to string as they are symbols
       end 
-      
-      if !pigeon_data[name][attributes_key]
-        pigeon_data[name][attributes_key] = []
-      end 
-        pigeon_data[name][attributes_key] << properties.to_s
-        binding.pry
-      end 
-    end
+    end 
   end 
   pigeon_data
 end
