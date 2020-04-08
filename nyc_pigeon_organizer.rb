0741b447d7d384fd51a4666ce18b958c80c8d13d
Omowonuola Jimoh <ojimoh21@live.co.uk>
2020-04-08 12:51:58 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,15 +1,23 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  pigeon_list = {}
+pigeon_data = {}
 
-  data.each do |attribute_name, attributes|
-    attributes.each do |attribute_value, pigeon_names|
-      pigeon_names.each do |name|
-        pigeon_list[name] ||= {}
-        pigeon_list[name][attribute_name] ||= []
-        pigeon_list[name][attribute_name].push(attribute_value.to_s)
-      end
-    end 
-  end
-
-  pigeon_list
+data.each do |attributes_key, attribute_values_array|
+  attribute_values_array.each do |properties, names|
+    
+    names.each do |name|
+      if !pigeon_data[name] 
+        pigeon_data[name] = {}
+      end 
+      
+      if !pigeon_data[name][attributes_key]
+        pigeon_data[name][attributes_key] = []
+      end 
+        pigeon_data[name][attributes_key] << properties.to_s
+        binding.pry
+      end 
+    end
+  end 
+  pigeon_data
 end
